de90125494118f45d5a92765e56a262c051433ad
Moretav <juniormoreta11@gmail.com>
2023-07-27 17:30:50 -0500

    j

--- script.js
+++ script.js
@@ -1,151 +1,78 @@
+// ******************************* 
+// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
 
-/*var message="in global";
-console.log("global:message=" + message);
-
-var a =function () {
-  var message = "inside a";
-  console.log("a: message =" + message);
-
-  function b (){
-    console.log("b: message =" +message);
-
- }
- b();
-}
- 
- a();
- */
-
- //lectured 42
-/*var x;
-console.log(x);
-
-if (x==undefined) {
-    console.log("x is undefined");
-}
-
-x=5;
-if (x==undefined){
-    console.log("x is still defined");
-}
-else{
-    console.log("x has been redefined to "+5);
-
-}*/
-
-//lecture
-
-/*var string = "helow";
-string += "world";
-// string =string +"world";
-console.log(string + "!");
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
 
 */
 
-/*
-console.log(( 5 + 4) /3);
-console.log(undefined /5);
-function test1 (a){
-    console.log(a/5);
-}
-test1();*/
+// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
 
-//***equality  */
-/*var x=4, y=4;
- if( x == y ){
-    console.log("x=4 is equal to y=4");
- }
+//* i remuved the funtion on this part
 
-x=4 ;
- if( x == y ){
-    console.log("x='4' is equal to y=4");
- }
 
-//***strict equality 
-if( x == y ){
-    console.log("Strict: x='4' is equal to y=4");
- }
- else{
-    console.log(" strict : x='4' in not equal to y=4");
- 
- }
- */
- /*if statement( al false)
- if(false||null ||undefined||""||0||NaN){
-    console.log("tthis line won't everr execute");
- }
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+     console.log(names);
+    
 
- else{ console.log(" all false");
-}
-
-//if  statement(all true)
-if(treu&&"hello"&&1&& -1&& "false"){
-    console.log("all true");
-}
-*/
-
-/*
-//bets practices for { style}
-//curly brace on the same or next line
-//is it just a style?
-function a(){
-    return{
-        name:"junior"
-    };
-}
-
-function b(){
-    return{
-        name:"junior e"
-    };
-}
-
-console.log(a());
-console.log(b());*/
- // for loop
-/*var sum=0;
-for (var i=0; i < 10; i++){
-    sum=sum +i;
-}
-console.log("sum of 0 through 9 is:" +sum);
-*/
-/*
-//import para js code en wix example 1
-function orderchickenwith(sideDish){
-    if(sideDish=== undefined) {
-        sideDish="whatever";
+    // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
+    // Loop over the names array and say either 'Hello' or "Good Bye"
+    // using either the helloSpeaker's or byeSpeaker's 'speak' method.
+    // See Lecture 50, part 1
+    for (var i = 0; i < names.length; i++) {
+       console.log("hello " + names[i]);
     }
-    console.log("Chichen with" + sideDish);   
-}
+     names[100]="jim";
 
-orderchickenwith("noodles");
-orderchickenwith();
+     for (var i = 0; i < names.length; i++) {
+        console.log("hello " + names[i]);
 
-//import para js code en wix example 2
-function orderchickenwith(sideDish){
-    sideDish = sideDish||"whatever!" 
-    console.log("Chichen with" + sideDish);   
-}
-
-orderchickenwith("noodles");
-orderchickenwith();
-*/
-// object creation
-var company=new Object();
-    company.name="facebook";
-    company.ceo= new Object();
-    company.ceo.firstName="Mark";
-    company.ceo.lastName="Zuckerr";
-    company.ceo.favColor="blue";
-
-    console.log(company);
-    console.log("company ceo name is:" +
-     company.ceo.firstName)
-     console.log(company["name"]);
-     company["$stock of company"]=110;
-
-     console.log("stock price is:" +company[stockPropName]);
-
-
-
-     
+    
+      // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
+      // Retrieve the first letter of the current name in the loop.
+      // Use the string object's 'charAt' function. Since we are looking for
+      // names that start with either upper case or lower case 'J'/'j', call
+      // string object's 'toLowerCase' method on the result so we can compare
+      // to lower case character 'j' afterwards.
+      // Look up these methods on Mozilla Developer Network web site if needed.
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+      // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+      // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+      // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+      // name in the loop.
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    
